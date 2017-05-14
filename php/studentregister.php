5ac8975d9e088c04e8cf487fda361e678cf84a67
robertoyoc <a01172599@itesm.mx>
2017-05-14 13:56:11 -0500

    added conexion.php

--- php/studentregister.php
+++ php/studentregister.php
@@ -1,12 +1,5 @@
 <?php
-$enlace = mysqli_connect("localhost", "proyectofinal", "roberto", "proyectofinal");
-
-	if (!$enlace) {
-    	echo "Error: No se pudo conectar a MySQL." . PHP_EOL;
-    	echo "errno de depuración: " . mysqli_connect_errno() . PHP_EOL;
-    	echo "error de depuración: " . mysqli_connect_error() . PHP_EOL;
-    	exit;
-	}
+	require 'conexion.php';
 
 	$matricula = $_POST['matricula'];
 	$nombre = $_POST['nombre'];
--- php/studentsearch.php
+++ php/studentsearch.php
@@ -1,13 +1,5 @@
 <?php
-$enlace = mysqli_connect("localhost", "proyectofinal", "roberto", "proyectofinal");
-
-	if (!$enlace) {
-    	echo "Error: No se pudo conectar a MySQL." . PHP_EOL;
-    	echo "errno de depuración: " . mysqli_connect_errno() . PHP_EOL;
-    	echo "error de depuración: " . mysqli_connect_error() . PHP_EOL;
-    	exit;
-	}
-
+	require 'conexion.php';
 	$matricula = $_POST['matricula'];
 
 	$query = "select * from alumno where matricula = '$matricula'";
