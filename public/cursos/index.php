022c341ab580641ecda83f9e455c3e7ceddbaf2b
Kevin Barrera <kev97barrera@gmail.com>
2017-05-14 14:35:13 -0500

    Añadí contenedores

--- public/cursos/index.php
+++ public/cursos/index.php
@@ -25,7 +25,29 @@
 
 		</ul>
 </header>
-	
+<section class="container">
+	<section id="initial-data">
+		<h3>Registrar un nuevo curso</h3>
+		<form action="">
+			<p>Nombre:</p><input type="text" name="name" id="name" required><br>
+			<select id="carreras">
+				<option value="IT">IT</option>
+				<option value="IS">IS</option>
+				<option value="CP">CP</option>
+				<option value="LA">LA</option>
+			</select>
+			<p>Clave:</p><input type="text" name="id" id="matrícula" required><br>
+			<p>Instructor:</p><input type="text" name="instructor" id="instructor" required><br>
+			<input type="submit" value="Registrar">
+		</form>
+	</section>
+	<section id="search">
+		<h3>Buscar información del curso</h3>
+		<form onsubmit="return search()">
+			<p>Calve:</p><input type="text" id="">
+		</form>
+	</section>
+</section>
 
 </body>
 </html>
